a190201fad92130c7aa0b512421c165ec69a06e5
ns <portification@gmail.com>
2019-10-27 11:09:03 +0100

    v1.0 of respawning

---
+++ mods/respawning/README.md
@@ -0,0 +1,29 @@
+Author: ns
+https://nssm.me
+
+Version: 1.0
+
+# Respawning adventurers
+
+When your teammates fail to revive you and your death counter runs out,
+you respawn at the start of the level instead of becoming a ghost.
+By default, you drop your items, and will have to recover them.
+
+## Server commands
+
+### `/TrulyGiveUp` (admin only)
+
+Forfeit the run, causing a game over regardless of respawning being enabled.
+
+## Preferences
+
+`$Pref::Server::Mods::RespawningEnabled` (boolean)  
+Default value: `true`
+Should players respawn? If disabled, they will just become ghosts.  
+
+`$Pref::Server::Mods::RespawningItems`  
+Default value: `"drop-no-new"`  
+What should happen to the items of players that die?  
+`"keep"`: Items are not dropped on death, and so are kept on respawn.  
+`"drop-no-new"`: Items are dropped on death, without giving items on respawn.  
+`"drop-new"`: Items are dropped on death, and new items are given on respawn.
---
+++ mods/respawning/server.cs
@@ -0,0 +1,118 @@
+$ModVersion[respawning] = 1;
+
+if (!$ModLoaded_Server[coresupport] &&
+	isFile("Add-Ons/mods/coresupport/server.cs"))
+{
+	exec("Add-Ons/mods/coresupport/server.cs");
+}
+
+if ($ModVersion[coresupport] $= 1 && $ModLoaded_Server[coresupport])
+{
+	registerServerCommand("TrulyGiveUp", "Give up on the run and end it.",
+		"", "adminonly");
+}
+
+if ($Pref::Server::Mods::RespawningEnabled $= "")
+{
+	$Pref::Server::Mods::RespawningEnabled = true;
+}
+
+if ($Pref::Server::Mods::RespawningItems $= "")
+{
+	// keep: Items don't drop, kept on respawn.
+	// drop-no-new: Items drop, no new items given on respawn.
+	// drop-new: Items drop, new items given on respawn.
+	$Pref::Server::Mods::RespawningItems = "drop-no-new";
+}
+
+function respawning_respawnClient(%client, %giveNewItems)
+{
+	if (isObject(%client.ghostPlayer))
+	{
+		%client.ghostPlayer.delete();
+		%client.ghostPlayer = 0;
+	}
+
+	if (isObject(%client.player))
+	{
+		%client.player.delete();
+		%client.player = 0;
+	}
+
+	%client.setStartingHealth();
+
+	if (%giveNewItems)
+	{
+		%client.createInventory();
+	}
+
+	%client.spawnPlayer();
+	%client.setSpectate(0);
+	%client.updateStats();
+}
+
+function serverCmdTrulyGiveUp(%client)
+{
+	if (!%client.isAdmin)
+	{
+		messageClient(%client, '',
+			"<color:ff0000>You must be a server admin to use " @
+			"<color:ffff66>/TrulyGiveUp<color:ff0000>.");
+		return;
+	}
+
+	setGameOverDeathMessage("Forfeited",
+		%client.nameBase @ " gave up on the run.");
+	doGameOverScreen(true);
+}
+
+package RespawningServer
+{
+	function Player::die(%player)
+	{
+		if ($Pref::Server::Mods::RespawningItems !$= "keep")
+		{
+			if (isObject(%player.client.inventory))
+			{
+				%player.client.inventory.dropAllItems();
+			}
+		}
+
+		return Parent::die(%player);
+	}
+
+	function GameConnection::doRealDeath(%client)
+	{
+		if (!$Pref::Server::Mods::RespawningEnabled)
+		{
+			return Parent::doRealDeath(%client);
+		}
+
+		respawning_respawnClient(%client,
+			$Pref::Server::Mods::RespawningItems $= "drop-new");
+	}
+
+	function checkForGameOverState()
+	{
+		return !$Pref::Server::Mods::RespawningEnabled &&
+			Parent::checkForGameOverState();
+	}
+
+	function gameOverCheck()
+	{
+		return !$Pref::Server::Mods::RespawningEnabled &&
+			Parent::gameOverCheck();
+	}
+
+	function doGameOverScreen(%force)
+	{
+		if (!$Pref::Server::Mods::RespawningEnabled || %force)
+		{
+			return Parent::doGameOverScreen();
+		}
+	}
+};
+
+activatePackage(RespawningServer);
+
+$ModLoaded_Server[respawning] = 1;
